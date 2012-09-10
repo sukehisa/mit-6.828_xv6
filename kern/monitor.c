6415f0ec5aefef2ed0c19dfa579ee282cdc0e477
Yusuke Kumura <yusuke@ny.ics.keio.ac.jp>
2012-09-10 15:28:54 +0900

    Here is my solution to lab2

--- kern/monitor.c
+++ kern/monitor.c
@@ -59,7 +59,18 @@ mon_kerninfo(int argc, char **argv, struct Trapframe *tf)
 int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
+	extern char *bootstacktop;
+
 	// Your code here.
+	int *p = (int *) read_ebp();  // ebp of current function which means 'mon_backtrace'
+	cprintf("read_ebp: %08x\n read_esp: %08x\n read_eip %08x\n", read_ebp(), read_esp(), read_eip());
+	cprintf("bootstacktop: %08x\n", bootstacktop);
+
+	while (p < (int *) bootstacktop) {
+		p = (int *)*p;
+		cprintf("ebp %08x eip %08x args %08x %08x %08x %08x %08x \n", p, p, *(p-4), *(p-3), *(p-2), *(p-1), *p);
+	}
+	
 	return 0;
 }
 
